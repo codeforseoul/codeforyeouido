00ad43a1d15a0c99ca28619ba515b985bbafd2f1
mwkang <mail@mwkang.pro>
2014-10-25 15:44:36 +0900

    파라미터 추가

--- api/controller/page.js
+++ api/controller/page.js
@@ -1,17 +1,24 @@
 var contentPage_path = 'content/'
 
-exports.home = function(req, res){
-    return res.render('home.ejs', {text : "hello world"});
+exports.home = function(req, res) {
+	return res.render('home.ejs', {
+		item1 : "hello world",
+		item2 : "item2",
+		item3 : "item3",
+		item4 : "item4",
+		item5 : "item5",
+		item6 : "item6"
+	});
 }
 
-exports.timeline = function(req, res){
-    return res.render('timeline.ejs', {});
+exports.timeline = function(req, res) {
+	return res.render('timeline.ejs', {});
 }
 
-exports.keyword = function(req, res){
-    return res.render('keyword.ejs', {});
+exports.keyword = function(req, res) {
+	return res.render('keyword.ejs', {});
 }
 
-exports.name = function(req, res){
-    return res.render('name.ejs', {});
-}   
+exports.name = function(req, res) {
+	return res.render('name.ejs', {});
+}
